52bc9d83c15720b6cdef7dd57e3b4ad7aca7c63b
alexey <pirojok167@gmail.com>
2022-01-10 22:43:27 +0300

    add conference page

--- src/Controller/ConferenceController.php
+++ src/Controller/ConferenceController.php
@@ -2,28 +2,36 @@
 
 namespace App\Controller;
 
+use App\Entity\Conference;
+use App\Repository\CommentRepository;
+use App\Repository\ConferenceRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Twig\Environment;
 
 class ConferenceController extends AbstractController
 {
     #[Route('/', name: 'homepage')]
-    public function index(Request $request): Response
+    public function index(Environment $twig, ConferenceRepository $conferenceRepository): Response
     {
-        $name = $request->query->get('hello');
+        return new Response($twig->render('conference/index.html.twig', [
+            'conferences' => $conferenceRepository->findAll(),
+        ]));
+    }
 
-        $greet = $name !== null ? sprintf('<h1>Hello %s!</h1>', htmlspecialchars($name)) : '';
+    #[Route('/conference/{id}', name: 'conference')]
+    public function show(Request $request, Environment $twig, Conference $conference, CommentRepository $commentRepository): Response
+    {
+        $offset = max(0, $request->query->getInt('offset', 0));
+        $paginator = $commentRepository->getCommentPaginator($conference, $offset);
 
-        return new Response(<<<EOF
-        <html>
-            <body>
-                $greet
-                <img src="/images/under-construction.png" />
-            </body>
-        </html>
-        EOF
-        );
+        return new Response($twig->render('conference/show.html.twig', [
+            'conference' => $conference,
+            'comments' => $paginator,
+            'previous' => $offset - CommentRepository::PAGINATOR_PER_PAGE,
+            'next' => min(count($paginator), $offset + CommentRepository::PAGINATOR_PER_PAGE),
+        ]));
     }
 }
