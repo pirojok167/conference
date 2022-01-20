340cbcce8041fad508727a1ed4c0cab685def93d
alexey <pirojok167@gmail.com>
2022-01-20 12:09:32 +0300

    comment workflow

--- src/MessageHandler/CommentMessageHandler.php
+++ src/MessageHandler/CommentMessageHandler.php
@@ -6,7 +6,10 @@ use App\Message\CommentMessage;
 use App\Repository\CommentRepository;
 use App\SpamChecker;
 use Doctrine\ORM\EntityManagerInterface;
+use Psr\Log\LoggerInterface;
 use Symfony\Component\Messenger\Handler\MessageHandlerInterface;
+use Symfony\Component\Messenger\MessageBusInterface;
+use Symfony\Component\Workflow\WorkflowInterface;
 use Symfony\Contracts\HttpClient\Exception\TransportExceptionInterface;
 
 class CommentMessageHandler implements MessageHandlerInterface
@@ -14,15 +17,25 @@ class CommentMessageHandler implements MessageHandlerInterface
     private SpamChecker $spamChecker;
     private EntityManagerInterface $entityManager;
     private CommentRepository $commentRepository;
+    private MessageBusInterface $messageBus;
+    private WorkflowInterface $commentStateMachine;
+    private LoggerInterface $logger;
 
     public function __construct(
         EntityManagerInterface $entityManager,
-        SpamChecker $spamChecker,
-        CommentRepository $commentRepository
-    ) {
+        SpamChecker            $spamChecker,
+        CommentRepository      $commentRepository,
+        MessageBusInterface    $messageBus,
+        WorkflowInterface      $commentStateMachine,
+        ?LoggerInterface       $logger = null
+    )
+    {
         $this->entityManager = $entityManager;
         $this->spamChecker = $spamChecker;
         $this->commentRepository = $commentRepository;
+        $this->messageBus = $messageBus;
+        $this->commentStateMachine = $commentStateMachine;
+        $this->logger = $logger;
     }
 
     /**
@@ -35,12 +48,23 @@ class CommentMessageHandler implements MessageHandlerInterface
             return;
         }
 
-        if ($this->spamChecker->getSpamScore($comment, $message->getContext()) === 2) {
-            $comment->setState('spam');
-        } else {
-            $comment->setState('published');
-        }
+        if ($this->commentStateMachine->can($comment, 'accept')) {
+            $score = $this->spamChecker->getSpamScore($comment, $message->getContext());
+            $transition = 'accept';
+            if (2 === $score) {
+                $transition = 'reject_spam';
+            } elseif (1 === $score) {
+                $transition = 'might_be_spam';
+            }
+            $this->commentStateMachine->apply($comment, $transition);
+            $this->entityManager->flush();
 
-        $this->entityManager->flush();
+            $this->messageBus->dispatch($message);
+        } elseif ($this->commentStateMachine->can($comment, 'publish') || $this->commentStateMachine->can($comment, 'publish_ham')) {
+            $this->commentStateMachine->apply($comment, $this->commentStateMachine->can($comment, 'publish') ? 'publish' : 'publish_ham');
+            $this->entityManager->flush();
+        } elseif ($this->logger !== null) {
+            $this->logger->debug('Dropping comment message', ['comment' => $comment->getId(), 'state' => $comment->getState()]);
+        }
     }
 }
