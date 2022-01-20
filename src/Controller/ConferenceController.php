c8eaf537bb1e804488bb295230b3637c64e8e361
alexey <pirojok167@gmail.com>
2022-01-20 17:30:42 +0300

    add frontend

--- src/Controller/ConferenceController.php
+++ src/Controller/ConferenceController.php
@@ -61,7 +61,7 @@ class ConferenceController extends AbstractController
             'conferences' => $conferenceRepository->findAll(),
         ]));
 
-        $response->setSharedMaxAge(3600);
+//        $response->setSharedMaxAge(3600);
 
         return $response;
     }
